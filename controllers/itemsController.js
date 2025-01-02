3fd04f4ad34d1cf6ebc1408ebdb3a88d806ca556
Portavion <156545430+Portavion@users.noreply.github.com>
2025-01-02 09:41:39 +0000

    feat: deleting categories

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -14,15 +14,52 @@ function addNewItemGet(req, res) {
 async function addNewItemPost(req, res) {
   const storageId = req.params.storageId;
   const itemName = req.body.itemName;
+  const itemQuantity = req.body.quantity;
   const queryPushNewItemText =
     "INSERT INTO item (item_name) VALUES ($1) RETURNING id";
+
   const result = await db.query(queryPushNewItemText, [itemName]);
   const newItemId = result.rows[0].id;
-  console.log("storageId:" + storageId + " new item id: " + newItemId);
   const queryPushNewStorageItemText =
-    "INSERT INTO storage_items (storage_id, item_id, quantity_total, quantity_left) VALUES ($1,$2,1,1)";
-  await db.query(queryPushNewStorageItemText, [storageId, newItemId]);
+    "INSERT INTO storage_items (storage_id, item_id, quantity_total, quantity_left) VALUES ($1,$2,1,$3)";
+  await db.query(queryPushNewStorageItemText, [
+    storageId,
+    newItemId,
+    itemQuantity,
+  ]);
   res.redirect("/storage/" + storageId);
 }
 
-module.exports = { itemGet, addNewItemGet, addNewItemPost };
+async function deleteItemGet(req, res) {
+  const itemId = req.params.itemId;
+  const deleteFromItemQueryText = "DELETE FROM item WHERE id = ($1);";
+  const deleteFromStorageItemQueryText =
+    "DELETE FROM storage_items WHERE item_id = ($1) RETURNING storage_id;";
+
+  const result = await db.query(deleteFromStorageItemQueryText, [itemId]);
+  const redirectToStorageId = result.rows[0].storage_id;
+
+  await db.query(deleteFromItemQueryText, [itemId]);
+  res.redirect("/storage/" + redirectToStorageId);
+}
+
+async function updateQuantityPost(req, res) {
+  const itemId = req.params.itemId;
+  const newQuantity = req.body.newQuantity;
+  console.log(newQuantity);
+  const updateQuantityQueryText =
+    "UPDATE storage_items SET quantity_left = $1 WHERE item_id = $2 RETURNING storage_id";
+
+  const result = await db.query(updateQuantityQueryText, [newQuantity, itemId]);
+  const redirectToStorageId = result.rows[0].storage_id;
+
+  res.redirect("/storage/" + redirectToStorageId);
+}
+
+module.exports = {
+  itemGet,
+  addNewItemGet,
+  addNewItemPost,
+  deleteItemGet,
+  updateQuantityPost,
+};
--- routes/itemsRouter.js
+++ routes/itemsRouter.js
@@ -5,6 +5,11 @@ const itemsRouter = Router();
 /* GET home page. */
 itemsRouter.get("/itemDetails/:itemId", itemsController.itemGet);
 itemsRouter.get("/addNewItem/:storageId", itemsController.addNewItemGet);
+itemsRouter.get("/deleteItem/:itemId", itemsController.deleteItemGet);
+itemsRouter.post(
+  "/updateQuantityPost/:itemId",
+  itemsController.updateQuantityPost,
+);
 
 itemsRouter.post("/addNewItem/:storageId", itemsController.addNewItemPost);
 
